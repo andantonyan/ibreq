f3d9f39d13f09a4d30a6ea2536bbda194050a51c
andantonyan <andantonyan@gmail.com>
2020-10-06 03:03:29 +0400

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,17 @@ pub type ConfigMap = HashMap<String, String>;
 #[macro_export]
 #[cfg(debug_assertions)]
 macro_rules! debug {
-    ($x:expr) => { dbg!($x) }
+  ($x:expr) => {
+    dbg!($x)
+  };
 }
 
 #[macro_export]
 #[cfg(not(debug_assertions))]
 macro_rules! debug {
-    ($x:expr) => { std::convert::identity($x) }
+  ($x:expr) => {
+    std::convert::identity($x)
+  };
 }
 
 #[derive(Debug, Clone)]
