0140b82efe30bdb03cdb1c0bb554afa21c895b26
andantonyan <andantonyan@gmail.com>
2020-10-15 00:15:13 +0400

    typo

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+echo -e "Building ibreq..."
 cargo build --release -p ibreq
 strip target/release/ibreq
 du -h target/release/ibreq
+echo -e "Done building ibreq.\n"
+
+echo -e "Building keyrec..."
+cargo build --release -p keyrec
+strip target/release/keyrec
+du -h target/release/keyrec
+echo -e "Done building keyrec.\n"
