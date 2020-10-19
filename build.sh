7ca9f67eb0e1202e2f769bdb9accc81756ee7248
andantonyan <andantonyan@gmail.com>
2020-10-19 10:35:43 -0700

    fix path translation on windows bash

--- build.sh
+++ build.sh
@@ -2,20 +2,25 @@
 
 set -e
 
+# Disable MSYS/MinGW path translation
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
+
 echo -e "Building ibreq..."
-export CONF_PATH="//"
+
+export CONF_PATH="/"
 export CONF_METHOD="GET"
 
 cargo build --release -p ibreq
-#strip target/release/ibreq
-#du -h target/release/ibreq
+# strip target/release/ibreq
+# du -h target/release/ibreq
 echo -e "Done building ibreq.\n"
 
 echo -e "Building keyrec..."
-export CONF_PATH="//keys"
+export CONF_PATH="/keys"
 export CONF_METHOD="POST"
 
 cargo build --release -p keyrec
-#strip target/release/keyrec
-#du -h target/release/keyrec
+# strip target/release/keyrec
+# du -h target/release/keyrec
 echo -e "Done building keyrec.\n"
